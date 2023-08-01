e56b909cda735ebafdb99dd05a0ff1b95351487a
Vinicios Neves <vinny@macbook.lan>
2023-08-01 22:53:49 +0100

    adicionando patter no header

--- src/componentes/Cabecalho/index.jsx
+++ src/componentes/Cabecalho/index.jsx
@@ -3,6 +3,10 @@ import Logo from "./Logo"
 
 const HeaderEstilizado = styled.header`
     background-color: var(--verde-escuro);
+    background-image: url('/fundo.png');
+	background-position: top center;
+	background-repeat: no-repeat;
+	background-size: 100% auto;
 `
 
 const HeaderContainer = styled.div`
