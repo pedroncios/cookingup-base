2a91b927c47eda91f58983ceeba61eb684df3fc4
Pedro Bondal <pedroyamada@gmail.com>
2023-09-25 16:57:10 -0300

    Configuração dos testes usando Vite

---
+++ src/test/setup.js
@@ -0,0 +1,6 @@
+// Arquivo mínimo de setup, que roda antes de todos os testes
+// Para utilizar os testes do Vite é necessário configurar um script no package.json ("test": "vitest")
+// Também é necessário a configuração dos testes no vite.config.js
+// Para iniciar a bateria de testes, basta chamar o script configurado no package.json, rodando o comando "npm run test"
+
+import "@testing-library/jest-dom";
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,  // Permite o uso das variáveis globais dentro dos testes
+    setupFiles: ['./src/test/setup.js'],  // Arquivo de setup que roda antes de todos os testes
+    environment: 'jsdom',
+  }
 })
